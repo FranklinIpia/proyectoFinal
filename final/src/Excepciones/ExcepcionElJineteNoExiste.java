0b1b715042702bf7e882e4b80aec59f8399381e4
Autorizado <Autorizado@HP>
2018-11-23 23:48:17 -0500

    :D

---
+++ final/src/Excepciones/ExcepcionElJineteNoExiste.java
@@ -0,0 +1,30 @@
+package Excepciones;
+
+public class ExcepcionElJineteNoExiste extends Exception{
+
+	public ExcepcionElJineteNoExiste() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExcepcionElJineteNoExiste(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExcepcionElJineteNoExiste(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExcepcionElJineteNoExiste(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ExcepcionElJineteNoExiste(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
